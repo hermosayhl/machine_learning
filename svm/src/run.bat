0c3712470a2d3cc7a7452d5f9937c679ed9421d4
hermosayhl <2535894692@qq.com>
2022-03-08 15:13:16 +0800

    Matplotlib-cpp 测试成功, 做个备份

---
+++ svm/src/run.bat
@@ -0,0 +1,3 @@
+g++ svm.cpp -std=c++11 -I D:\environments\C++\3rdparty\matplotlib -I D:\environments\Miniconda\include -I D:\environments\Miniconda\ -ID:\environments\Miniconda\Lib\site-packages\numpy\core\include -LD:\environments\Miniconda\libs -lpython37 -o run.exe
+run.exe
+pause
