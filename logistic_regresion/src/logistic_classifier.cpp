86979afe928cd2474e5b7e42da2c8abb37b53902
hermosayhl <2535894692@qq.com>
2022-03-25 15:24:06 +0800

    画了画 ROC 曲线

--- logistic_regresion/src/logistic_classifier.cpp
+++ logistic_regresion/src/logistic_classifier.cpp
@@ -30,8 +30,11 @@ inline bool all_zero(const feature_type& w, const data_type eps) {
     return true;
 }
 
+double exp_limit = std::exp(60);
 
 inline double fast_exp(const double y) {
+    // if(y >= 60) return exp_limit;
+    // else if(y <= -60) return 0;
     double d;
     *(reinterpret_cast<int*>(&d) + 0) = 0;
     *(reinterpret_cast<int*>(&d) + 1) = static_cast<int>(1512775 * y + 1072632447);
@@ -45,7 +48,6 @@ private:
     data_type bias;
     int dimension;
 public:
-
     std::vector< std::pair<int, data_type> > predict(const std::vector<feature_type>& input) {
         const int samples_num = input.size();
         std::vector< std::pair<int, data_type> > prediction;
@@ -60,13 +62,70 @@ public:
         return prediction;
     }
 
-    void score(const std::vector<feature_type>& input, const std::vector<int>& label) {
+    void score(const std::vector<feature_type>& input, const std::vector<int>& label, const bool plot=false) {
+        std::cout << "开始评测分类器性能......\n";
         const auto prediction = this->predict(input);
-        int correct = 0;
+        // 计算混淆矩阵
+        int confusion_matrix[2][2] = {0};
         const int samples_num = label.size();
         for(int i = 0;i < samples_num; ++i)
-            correct += prediction[i].first == label[i];
-        printf("%d/%d====> [accuracy  %.3f]\n", correct, samples_num, correct * 1.0 / samples_num);
+            ++confusion_matrix[label[i]][prediction[i].first], std::cout << label[i] << std::endl;
+        // 打印混淆矩阵
+        std::cout << "混淆矩阵如下(行 label, 列 prediction, 0 negative, 1 positive) : \n";
+        for(int i = 0;i < 2; ++i) {
+            std::cout << "\t";
+            for(int j = 0;j < 2; ++j)
+                std::cout << confusion_matrix[i][j] << "\t";
+            std::cout << std::endl;
+        }
+        auto& M = confusion_matrix;
+        // 计算正确率, 精准率和召回率, 以及 f1
+        data_type precision = M[1][1] * 1. / (M[1][1] + M[0][1]);
+        data_type recall = M[1][1] * 1. / (M[1][1] + M[1][0]);
+        printf("正类===> [precision %.3f] [recall %.3f] [f1 %.3f]\n",
+               precision, recall, 2 * precision * recall / (precision + recall));
+        precision = M[0][0] * 1. / (M[0][0] + M[1][0]);
+        recall = M[0][0] * 1. / (M[0][0] + M[0][1]);
+        printf("负类===> [precision %.3f] [recall %.3f] [f1 %.3f]\n",
+               precision, recall, 2 * precision * recall / (precision + recall));
+        const int correct_nums = M[0][0] + M[1][1];
+        printf("正确率 %d/%d====> [accuracy  %.3f]\n", correct_nums, samples_num, correct_nums * 1.0 / samples_num);
+        // 第一类错误率(1 判定为 0), 第二类错误率(0 判定为 1)
+        printf("第一类错误率  %.3f\n", M[0][1] * 1. / (M[0][1] + M[0][0]));
+        printf("第一类错误率  %.3f\n", M[1][0] * 1. / (M[1][0] + M[1][1]));
+        // 画 ROC 曲线
+        if(plot) {
+            this->plot_classifier(input, label, -1);
+            // 需要遍历一系列阈值, 然后进行判断
+            std::vector<data_type> TP, FP;
+            for(data_type thresh = 0; thresh <= 1.0; thresh += 0.0001) {
+                // 用这个阈值判断, 计算真阳率(1 的样本判断为 1), 假阳率(0 的样本判断为 1)
+                int _M[2][2] = {0};
+                for(int i = 0;i < samples_num; ++i) {
+                    // 取出这个样本是正样本的概率
+                    const data_type prob = prediction[i].second;
+                    // 如果是正样本的概率大于阈值, 则是 positive, 否则 negative
+                    const int pred = prob >= thresh ? positive: negative;
+                    // 填充新的混淆矩阵
+                    _M[label[i]][pred]++;
+                }
+                // 计算真阳率, 假阳率
+                const data_type true_positive = _M[1][1] * 1. / (_M[1][1] + _M[1][0]);
+                const data_type false_positive = _M[0][1] * 1. / (_M[0][1] + _M[0][0]);
+                // std::cout << thresh << "===> " << false_positive << " " << true_positive << std::endl;
+                TP.emplace_back(true_positive);
+                FP.emplace_back(false_positive);
+            }
+            std::cout << "AOC : waiting\n";
+            Py_SetPythonHome(L"D:/environments/Miniconda");
+            plt::plot(FP, TP, {{"c", "red"}});
+            plt::save("./output/ROC.png", 600);
+            plt::show();
+            plt::clf();
+            plt::cla();
+            plt::close();
+        }
+        // 跑一下数据不均衡的结果, 看看
     }
 
     data_type forward(const feature_type& x) {
@@ -128,7 +187,8 @@ public:
             loss_history[epoch - 1] = total_loss;
             std::cout << "epoch  " << epoch << "  loss  " << total_loss << std::endl;
         }
-        for(int i = 0;i < dimension; ++i) std::cout << weight[i] << std::endl;
+        std::cout << "weights = \n";
+        for(int i = 0;i < dimension; ++i) std::cout << "\t" << weight[i] << std::endl;
         std::cout << "bias = " << bias << std::endl;
         if(plot) {
             // 画损失函数
@@ -202,17 +262,15 @@ public:
  *
  * 1. l1 正则化是什么鬼, 而且 l2 正则化也没法体现出来, 得找个例子
  * 2. logsitic 回归可以处理近似线性可分
- * 3. logsitic 可以做除了分类之外的事情吗?
+ * 3. 不均衡样本
  * 4. 牛顿法的 logsitic
- * 5. 为什么叫 logsitic 回归, 跟线性回归有啥关系
  */
 
 
-int main() {
-    std::setbuf(stdout, 0);
 
+std::pair<std::vector<feature_type>, std::vector<int> > read_from_txt(const char* txt_path) {
     // 读取数据
-    std::ifstream reader("./test/test.txt");
+    std::ifstream reader(txt_path);
     // 读写
     int samples_num;
     reader >> samples_num;
@@ -223,19 +281,34 @@ int main() {
     std::vector<int> Y(samples_num, 0);
     for(int i = 0;i < samples_num; ++i) {
         reader >> a >> b >> c;
-        if(c != negative and c != positive) c = 0;
+        if(c == -1) c = 0;
         feature_type one(2);
         one[0] = a;
         one[1] = b;
         X.emplace_back(one);
+        std::cout << c << std::endl;
         Y[i] = c;
     }
     // 关闭文件资源
     reader.close();
+    return {X, Y};
+}
+
+
+int main() {
+    std::setbuf(stdout, 0);
+
+    auto train_data = read_from_txt("./test/test_soft.txt");
+    auto& X = train_data.first;
+    auto& Y = train_data.second;
     // 声明一个分类器
     LogisticClassifier classifier;
-    classifier.fit(X, Y, 200, 1e-2, {"l2", 1.0}, true, 212);
-    classifier.score(X, Y);
+    classifier.fit(X, Y, 200, 1e-2, {"none", 1.0}, true, 212);
+    classifier.score(X, Y, true);
+
+    // 不均衡的还无法展现效果
+//    auto test_data = read_from_txt("./test/test_soft_unbalance.txt");
+//    classifier.score(test_data.first, test_data.second, true);
 
     // 试试逻辑斯蒂回归可以得出类别的概率
     auto prediction = classifier.predict({{6.07, 0.33}, {2.88, -1.71}, {-0.74, -1.84}});
